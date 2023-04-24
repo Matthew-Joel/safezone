e3167740e4342b548cfc5ab2a4d54f94bb57ccaa
Okoye Emmanuel .M <mmesoemma@gmail.com>
2023-04-24 16:03:33 +0100

    Added Landing page and edited files

--- components/templates/homepage/index.tsx
+++ components/templates/homepage/index.tsx
@@ -1,10 +1,16 @@
 import NavLinks from "../../ui/layout/nav/NavLinks";
 import HomepageHero from "../../ui/organisms/homepage/hero";
 import StaySafe from "../../ui/organisms/homepage/stay-safe";
+import ProtectYou from "../../ui/organisms/homepage/protect-you";
+import BeSafe from "../../ui/organisms/homepage/be-safe";
+import ECommerce from "../../ui/organisms/homepage/e-commerce";
+import How from "../../ui/organisms/homepage/How";
+import ButWhy from "../../ui/organisms/homepage/but-why";
+import Join from "../../ui/organisms/homepage/join";
 
 const HomepageTemplate = () => {
 	return (
-		<>
+		<div className="w-[100vw] relative">
 			<HomepageHero />
 			<div className="sticky h-auto top-1 xl:-mt-36 lg:-mt-20 -mt-32 z-10 w-full">
 				<div className="flex justify-center">
@@ -12,7 +18,13 @@ const HomepageTemplate = () => {
 				</div>
 			</div>
 			<StaySafe />
-		</>
+			<ProtectYou />
+			<BeSafe />
+			<How/>
+			<ButWhy/>
+			<ECommerce />
+			<Join />
+		</div>
 	);
 };
 
--- components/ui/layout/footer/index.tsx
+++ components/ui/layout/footer/index.tsx
@@ -1,9 +1,58 @@
 import React from "react";
+import Link from "next/link";
 
 const Footer = () => {
 	return (
 		<footer>
-			<div>Footer</div>
+			<div className="mt-12 grid md:grid-cols-4 md:space-x-4 md:mx-14 mx-8 mb-12">
+				<div className="w-full flex flex-col gap-y-2 mt-5">
+					<div className="">
+						<Link href="/" className="w-32 lg:w-36 flex items-center">
+							<img
+								src="../images/safezone_logo_light.png"
+								className="h-auto w-auto lg:mr-10 object-contain"
+								alt="SafeZone Logo"
+							/>
+						</Link>
+					</div>
+					<div className="mt-2">
+						<p className="text-sm md:w-3/4">
+							It's better to be safe than sorry...
+							always request to pay with safezone.
+						</p>
+					</div>
+				</div>
+				<div className="w-full flex flex-col gap-y-2 mt-5">
+					<div className="">
+						<h2 className="font-bold">Company</h2>
+					</div>
+					<div className="space-y-3">
+						<p className="text-sm md:w-3/4">
+							No 29, Adeoluwa Street Oguloloa Junction Ilorin, Kwara State.
+						</p>
+						<p className="text-sm">About Us</p>
+					</div>
+				</div>
+				<div className="w-full flex flex-col gap-y-3 mt-5">
+					<div className="">
+						<h2 className="font-bold">Resources</h2>
+					</div>
+					<div className="md:space-y-2">
+						<p className="text-sm">Blog</p>
+						<p className="text-sm">Term and Privacy</p>
+					</div>
+				</div>
+				<div className="w-full flex flex-col gap-y-3 mt-5">
+					<div className="">
+						<h2 className="font-bold">Let's Connect</h2>
+					</div>
+					<div className="md:space-y-2">
+						<p className="text-sm">Facebook</p>
+						<p className="text-sm">LinkedIn</p>
+						<p className="text-sm">Twitter</p>
+					</div>
+				</div>
+			</div>
 		</footer>
 	);
 };
