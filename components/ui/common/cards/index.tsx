9da1d8866464b55e0b3f17f0483b63c778a619a3
“Josh <joshsavyy007@gmail.com>
2023-05-09 16:57:07 +0100

    fix: ui-changes - separated the blog cards into a seprate folder

--- components/ui/common/cards/index.tsx
+++ components/ui/common/cards/index.tsx
@@ -14,22 +14,30 @@ export const LatestBlogPostCard = (props: BlogPostCardType) => {
 					width={100}
 				/>
 			</div>
-			<div className="flex flex-col justify-center gap-4">
+			<div className="flex flex-col gap-6 justify-center">
 				<h2 className="text-xl font-bold mt-4">{props.title}</h2>
 				<p className="text-sm">{props.description}</p>
 				<div className="text-xs grid grid-cols-2 items-center gap-4">
-					<span>
+					<span className="text-zinc-500">
 						Posted on <span className="font-semibold">{props.createdAt}</span>
 					</span>
-					<span className="flex items-center gap-1">
+					<span className="flex items-center gap-1 text-zinc-500">
 						By: <span className="font-semibold">{props.createdBy}</span>
 					</span>
-					<Link href={props.link ? props.link : "#"}>
-						<div className="rounded-xl w-[40vw] lg:w-full border-2 border-[#00CCFF] bg-gradient-to-r from-[#00CCFF]/[0.3] to-[#80FF00]/[0.2] py-1 px-3 text-black mt-2 lg:mt-0">
-							Continue Reading
-						</div>
-					</Link>
 				</div>
+				<Link href={props.link ? props.link : "#"}>
+					<div
+						style={{
+							background:
+								"linear-gradient(221.99deg, rgba(0, 204, 255, 0.13) 10.93%, rgba(128, 255, 0, 0.13) 281.44%)",
+						}}
+						className="rounded-xl text-center text-sm border-2 border-l-[#00CCFF] border-t-[#00CCFF] 
+                        border-r-[#80FF00] border-b-[#80FF00] hover:border-l-[#80FF00] hover:border-r-[#00CCFF] hover:border-b-[#00CCFF] 
+                        hover:border-t-[#80FF00] transition-all hover:scale-[.96] duration-300 py-2 px-3 text-black mt-3 lg:mt-0"
+					>
+						Continue Reading
+					</div>
+				</Link>
 			</div>
 		</div>
 	);
--- components/ui/organisms/blogpage/latest/index.tsx
+++ components/ui/organisms/blogpage/latest/index.tsx
@@ -1,33 +1,36 @@
 import React from "react";
 import { LatestBlogPostCard } from "../../../common/cards";
+import { AnimationOnScroll } from "react-animation-on-scroll";
 
 const Latest = () => {
 	return (
 		<div className="bg-white relative lg:mx-20 mx-8 my-20 lg:mx-14">
 			<h1 className="text-2xl font-medium">Latest</h1>
 			<div className="flex justify-center">
-				<div className="grid md:grid-cols-2 xl:grid-cols-3 mt-10 gap-6">
-					<LatestBlogPostCard
-						title="5 Tips for Avoiding E-Commerce Scammers"
-						description={`Online shopping has become increasingly popular over the years, but
+				<AnimationOnScroll animateIn="animate__fadeInUp">
+					<div className="grid md:grid-cols-2 xl:grid-cols-3 mt-10 gap-6">
+						<LatestBlogPostCard
+							title="5 Tips for Avoiding E-Commerce Scammers"
+							description={`Online shopping has become increasingly popular over the years, but
 					unfortunately, so have e-commerce scammers. These scammers use various
 					tactics to trick shoppers into giving away their personal information or
 					money...`}
-						createdBy={"Darey Oloye"}
-						createdAt={"12/01/2023"}
-						link={`/blog/${"#id"}`}
-					/>
-					<LatestBlogPostCard
-						title="5 Tips for Avoiding E-Commerce Scammers"
-						description={`Online shopping has become increasingly popular over the years, but
+							createdBy={"Darey Oloye"}
+							createdAt={"12/01/2023"}
+							link={`/blog/${"#id"}`}
+						/>
+						<LatestBlogPostCard
+							title="5 Tips for Avoiding E-Commerce Scammers"
+							description={`Online shopping has become increasingly popular over the years, but
 					unfortunately, so have e-commerce scammers. These scammers use various
 					tactics to trick shoppers into giving away their personal information or
 					money...`}
-						createdBy={"Darey Oloye"}
-						createdAt={"12/01/2023"}
-						link={`/blog/${"#id"}`}
-					/>
-				</div>
+							createdBy={"Darey Oloye"}
+							createdAt={"12/01/2023"}
+							link={`/blog/${"#id"}`}
+						/>
+					</div>
+				</AnimationOnScroll>
 			</div>
 		</div>
 	);
