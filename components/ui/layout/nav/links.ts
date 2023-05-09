c9d6d1fee1c8a6a79f797283cc3d07495c246960
“Josh <joshsavyy007@gmail.com>
2023-05-09 16:00:57 +0100

    feat: added a page navigation loader

--- components/ui/layout/nav/links.ts
+++ components/ui/layout/nav/links.ts
@@ -2,6 +2,6 @@ export const navlinksChildren = [
 	{ title: "Home", link: "/" },
 	{ title: "What is SafeZone", link: "/#what_is_safezone" },
 	{ title: "Product", link: "/product" },
-	{ title: "Blog", link: "#" },
-	{ title: "Contact Us", link: "#contact" },
+	{ title: "Blog", link: "/blog" },
+	{ title: "Contact Us", link: "#" },
 ];
