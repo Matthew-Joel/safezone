16385dcf10e7d8d5dc025e9416a898678a6c8a54
Matthew-Joel <joelmathew826@gmail.com>
2023-05-23 11:46:08 +0100

    fixed-productpage-text-alignment

--- components/ui/organisms/productpage/hero/index.tsx
+++ components/ui/organisms/productpage/hero/index.tsx
@@ -5,8 +5,8 @@ const Hero = () => {
 	return (
 		<div className="bg-white h-auto lg:h-[80vh] w-screen lg:mt-12 mt-28 mb-10 relative">
 			<div className="md:flex justify-center">
-				<div className="lg:flex items-center">
-					<div className="flex items-center relative">
+				<div className="lg:flex items-center ml-20">
+					<div className="flex items-center relative -ml-20 md:ml-14">
 						<img
 							src="/images/logos/laptop-alone.png"
 							alt="hero"
@@ -21,14 +21,14 @@ const Hero = () => {
 					<p
 						className="animate__animated animate__fadeInRight md:text-left text-center font-medium mt-3 
 						md:mt-0 text-4xl md:text-5xl lg:w-1/2 text-transparent bg-clip-text
-						 px-6 bg-gradient-to-r from-[#00CCFF] to-[#80FF00]"
+						 px-10 -ml-20 bg-gradient-to-r from-[#00CCFF] to-[#80FF00] "
 					>
 						What are we building?
 					</p>
 				</div>
 			</div>
-			<div className="flex justify-center items-center xl:mx-[350px] md:mx-12 mt-5">
-				<p className="text-justify mx-3">
+			<div className="flex justify-center items-center xl:mx-[350px] md:mx-12 mt-5 ">
+				<p className="text-justify md:text-center mx-16 md:mx-[300px] text-sm">
 					Our team is in the process of developing a secure payment platform aimed at
 					safeguarding your hard-earned money or goods from fraudulent individuals
 					who present themselves as genuine sellers or buyers. By creating a secure
